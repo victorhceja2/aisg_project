a08a91ac776c77579a2f09d7453191543edd3172
RobertoMarcelino98 <skyhome.lrmr@gmail.com>
2025-05-21 10:57:12 -0600

    cambio de cliente

--- backend/app/routers/client.py
+++ backend/app/routers/client.py
@@ -12,18 +12,3 @@ router = APIRouter(
 @router.get("/")
 def get_clients(db: Session = Depends(get_db)):
     return db.query(Cliente).all()
-
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app.database import get_db
-from app.models import Client
-from app.schemas_clients import ClientResponse
-
-router = APIRouter(
-    prefix="/catalog/clients",
-    tags=["Clients"]
-)
-
-@router.get("/", response_model=list[ClientResponse])
-def get_clients(db: Session = Depends(get_db)):
-    return db.query(Client).all()
